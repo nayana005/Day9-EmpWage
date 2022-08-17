9e6a8f858294d3d9268743ffedf3ddb59a58b81b
nayana005 <nayanasuresh1005@gmail.com>
2022-08-17 22:36:02 +0530

    Added: Emp Present or Absent

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.emp_wage9;
+
+public class Main {
+	public static void main(String[] args) {
+
+        System.out.println("\nWelcome to Employee Wage Computation Program !\n");
+        OopsEmpWage.empCheck();
+    }
+}
+
+
